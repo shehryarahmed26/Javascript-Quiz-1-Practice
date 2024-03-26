37e8bdc8efc4cd59160ca4d29e1b7238e78d5dd7
Shehryar <shehryarahmed268@gmail.com>
2024-03-26 08:08:34 +0500

    Upload Files

---
+++ Chapter-4/script.js
@@ -0,0 +1,34 @@
+////////////////////////////// Assignment no 04 //////////////////////////////////////////// 
+
+//////////////////////////// question no 01 //////////////////////////////////////////////// 
+
+let name1, name2, name3 ;
+
+//////////////////////////// question no 02 //////////////////////////////////////////////// 
+
+// legal names of variables 
+
+let $book2;
+let user76;
+let _useremail
+let user;
+let User;
+
+// illegal names of variables
+
+// let user name
+// let #user
+// let @User
+// let 87email 
+// let !user 
+
+//////////////////////////// question no 03 //////////////////////////////////////////////// 
+
+
+document.write("<p> Variables names can only contain  , numbers, $ and _ . For example : $my_1stVariable <br> Variable must begin with a letter, $ or _ . For example : $name, _name or name <br> Variable names are case sensitive <br> Variables names should not be javascript keywords </p>")
+
+
+
+
+
+
---
+++ Chapter-5/script.js
@@ -0,0 +1,50 @@
+let num1;
+document.write(`<p> Value After Variable decleration is ${num1}</p>`)
+num1 = 8;
+document.write(`<p> Initial Value is ${num1}</p>`)
+num1++;
+document.write(`<p> Value after increment is ${num1}</p>`)
+num1 += 8;
+document.write(`<p> Value after addition is ${num1}</p>`)
+num1--;
+document.write(`<p> Value after decrement is ${num1}</p>`)
+num1 = num1 % 3
+document.write(`<p> The Remainder of Value is ${num1}</p>`)
+
+
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let ticketprice = 600;
+let buyingticket = 5;
+document.write(`The Cost of Buying ${5} tickets of a movie is ${buyingticket * ticketprice}`)
+
+
+
+    let table = 4;
+    let table2 = 1;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+    document.write(`<p>${table} x ${table2} = ${table * table2}</p>`) 
+    table2++;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    let celsiustemp = 
+    let
